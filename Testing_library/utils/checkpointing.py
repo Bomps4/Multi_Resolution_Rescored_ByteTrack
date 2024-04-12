13957519d352c5eb1de94f30d61a4bd4e40fe8a5
Luca Bompani <74559071+Bomps4@users.noreply.github.com>
2024-04-12 23:24:39 +0200

    Update checkpointing.py

--- Testing_library/utils/checkpointing.py
+++ Testing_library/utils/checkpointing.py
@@ -1,4 +1,5 @@
-
+# Copyright (c) 2021-2022 Megvii Inc. and its affiliates. 
+# modified by Bomps4 (luca.bompani5@unibo.it)  Copyright (C) 2023-2024 University of Bologna, Italy.
 
 from typing import Type
 import torch
